db49eaaa3d8b4892c04402c29b51ed7036ede7e0
JuliaOlowniuk <julia.olowniuk@gmail.com>
2024-04-27 08:25:15 +0200

    strona logowania

---
+++ app/src/main/java/com/example/myapplication/LoginPage.kt
@@ -0,0 +1,79 @@
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.Button
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+
+class LoginPage : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            LoginPageContent()
+        }
+    }
+}
+
+@Composable
+fun LoginPageContent() {
+    Surface(
+        color = Color.White,
+        modifier = Modifier.fillMaxSize(),
+    ) {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            val h4 = TextStyle(
+                fontWeight = FontWeight.Bold,
+                fontSize = 24.sp,
+                letterSpacing = 0.15.sp,
+            )
+
+            Text(
+                text = "Logowanie",
+                style = h4,
+                modifier = Modifier.padding(bottom = 16.dp)
+            )
+            var username by remember { mutableStateOf("") }
+            var password by remember { mutableStateOf("") }
+
+            TextField(
+                value = username,
+                onValueChange = { username = it },
+                label = { Text("Login") },
+                modifier = Modifier.padding(bottom = 16.dp),
+                keyboardOptions = KeyboardOptions.Default.copy(keyboardType = KeyboardType.Text)
+            )
+
+            TextField(
+                value = password,
+                onValueChange = { password = it },
+                label = { Text("Hasło") },
+                modifier = Modifier.padding(bottom = 16.dp),
+                keyboardOptions = KeyboardOptions.Default.copy(keyboardType = KeyboardType.Password)
+            )
+
+            Button(
+                onClick = { /* logowanie */ },
+                modifier = Modifier.padding(bottom = 16.dp)
+            ) {
+                Text("Zaloguj się")
+            }
+        }
+    }
+}
