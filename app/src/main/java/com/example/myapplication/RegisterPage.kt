4ed2655b90c9e0963bda6bcedfa2689123e7918f
JuliaOlowniuk <julia.olowniuk@gmail.com>
2024-04-27 08:28:32 +0200

    strona rejestracji

---
+++ app/src/main/java/com/example/myapplication/RegisterPage.kt
@@ -0,0 +1,90 @@
+package com.example.myapplication
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.Button
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+
+class RegisterPage : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            RegisterPageContent()
+        }
+    }
+}
+
+@Composable
+fun RegisterPageContent() {
+    Surface(
+        color = Color.White,
+        modifier = Modifier.fillMaxSize(),
+    ) {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            val h4 = TextStyle(
+                fontWeight = FontWeight.Bold,
+                fontSize = 24.sp,
+                letterSpacing = 0.15.sp,
+            )
+
+            Text(
+                text = "Rejestracja",
+                style = h4,
+                modifier = Modifier.padding(bottom = 16.dp)
+            )
+            var username by remember { mutableStateOf("") }
+            var email by remember { mutableStateOf("") }
+            var password by remember { mutableStateOf("") }
+
+            TextField(
+                value = username,
+                onValueChange = { username = it },
+                label = { Text("Login") },
+                modifier = Modifier.padding(bottom = 16.dp),
+                keyboardOptions = KeyboardOptions.Default.copy(keyboardType = KeyboardType.Text)
+            )
+
+            TextField(
+                value = email,
+                onValueChange = { email = it },
+                label = { Text("Email") },
+                modifier = Modifier.padding(bottom = 16.dp),
+                keyboardOptions = KeyboardOptions.Default.copy(keyboardType = KeyboardType.Email)
+            )
+
+            TextField(
+                value = password,
+                onValueChange = { password = it },
+                label = { Text("Hasło") },
+                modifier = Modifier.padding(bottom = 16.dp),
+                keyboardOptions = KeyboardOptions.Default.copy(keyboardType = KeyboardType.Password)
+            )
+
+            Button(
+                onClick = { /* rejestracja */ },
+                modifier = Modifier.padding(bottom = 16.dp)
+            ) {
+                Text("Zarejestruj się")
+            }
+        }
+    }
+}
+
