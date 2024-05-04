4b321ea171c39b216345c2d566636e97ea063737
JuliaOlowniuk <julia.olowniuk@gmail.com>
2024-05-04 17:03:59 +0200

    try regulamin

--- app/src/main/java/com/example/myapplication/MainActivityFragment.kt
+++ app/src/main/java/com/example/myapplication/MainActivityFragment.kt
@@ -7,7 +7,8 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import androidx.navigation.fragment.findNavController
-import com.example.myapplication.R
+
+
 
 class MainActivityFragment : Fragment() {
 
--- app/src/main/java/com/example/myapplication/RegulaminFragment.kt
+++ app/src/main/java/com/example/myapplication/RegulaminFragment.kt
@@ -1,18 +1,26 @@
-package com.example.myapplication
-
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.WebView
+import androidx.fragment.app.Fragment
+import com.example.myapplication.R
 
 class RegulaminFragment : Fragment() {
-
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_regulamin, container, false)
+        // Inflate the layout for this fragment
+        val view = inflater.inflate(R.layout.fragment_regulamin, container, false)
+
+        // Locate the WebView in fragment_regulamin.xml layout
+        val webView = view.findViewById<WebView>(R.id.webView)
+
+        // Load local PDF file from assets folder
+        webView.loadUrl("file:///android_asset/regulamin_kina_aurora.pdf")
+
+        return view
     }
 }
