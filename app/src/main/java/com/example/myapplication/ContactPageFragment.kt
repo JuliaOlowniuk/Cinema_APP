a68f330ba678593c4499251715866626598b2b0e
JuliaOlowniuk <julia.olowniuk@gmail.com>
2024-05-05 09:07:21 +0200

    map1

--- app/src/main/java/com/example/myapplication/ContactPageFragment.kt
+++ app/src/main/java/com/example/myapplication/ContactPageFragment.kt
@@ -4,7 +4,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
+import android.webkit.WebView
+import android.webkit.WebViewClient
 class ContactPageFragment : Fragment() {
 
     override fun onCreateView(
@@ -12,7 +13,18 @@ class ContactPageFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_contact_page, container, false)
-    }
+        val rootView = inflater.inflate(R.layout.fragment_contact_page, container, false)
+        val webView: WebView = rootView.findViewById(R.id.webView)
 
+        // Włącz obsługę JavaScript w WebView
+        webView.settings.javaScriptEnabled = true
+
+        // Ustaw klienta WebView, aby strony były wyświetlane w samym WebView, a nie w przeglądarce zewnętrznej
+        webView.webViewClient = WebViewClient()
+
+        // Załaduj URL mapy w WebView
+        webView.loadUrl("https://www.google.com/maps")
+
+        return rootView
+    }
 }
