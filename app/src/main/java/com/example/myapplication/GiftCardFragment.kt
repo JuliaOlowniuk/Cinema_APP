b1de9b5b9644419f4f2f217fcda205031e509d00
k4mpa15 <kampa.kampa@wp.pl>
2024-05-10 16:27:07 +0200

    start of front api

--- app/src/main/java/com/example/myapplication/GiftCardFragment.kt
+++ app/src/main/java/com/example/myapplication/GiftCardFragment.kt
@@ -5,6 +5,13 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.net.HttpURLConnection
+import java.net.URI
+import java.net.URL
+
 
 class GiftCardFragment : Fragment() {
 
@@ -12,9 +19,47 @@ class GiftCardFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
+
     ): View? {
-        // Inflate the layout for this fragment
+
+
         return inflater.inflate(R.layout.fragment_gift_card, container, false)
+    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val apiUrl = "http://localhost:8081/prices/giftcard"
+        val standardowaTextView = view.findViewById<TextView>(R.id.standardowa_text)
+        try {
+            val url : URL = URI.create(apiUrl).toURL()
+            val connection : HttpURLConnection = url.openConnection() as HttpURLConnection
+
+            connection.requestMethod = "GET"
+
+            val responseCode: Int = connection.responseCode
+            println("Response Code: $responseCode")
+
+            if (responseCode == HttpURLConnection.HTTP_OK) {
+                val reader : BufferedReader = BufferedReader(InputStreamReader(connection.inputStream))
+                var line: String?
+                val response = StringBuilder()
+
+                while (reader.readLine().also { line = it } != null) {
+                    response.append(line)
+                }
+
+                reader.close()
+
+
+                standardowaTextView.text = "Response Data: $response"
+            } else {
+                println("Error: Unable to fetch data from the API")
+            }
+
+            connection.disconnect()
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+
     }
 
 }
