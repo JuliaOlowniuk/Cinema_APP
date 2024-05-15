51135c5f783551dd85916eecdbeaac1734cb03b1
k4mpa15 <kampa.kampa@wp.pl>
2024-05-15 22:21:37 +0200

    logout button

--- app/src/main/java/com/example/myapplication/UserProfileFragment.kt
+++ app/src/main/java/com/example/myapplication/UserProfileFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.fragment.findNavController
 
 class UserProfileFragment : Fragment() {
 
@@ -13,8 +15,12 @@ class UserProfileFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_user_profile, container, false)
+        val view =  inflater.inflate(R.layout.fragment_user_profile, container, false)
+        val buttonloggout = view.findViewById<Button>(R.id.buttonLogout)
+        buttonloggout.setOnClickListener{
+            findNavController().navigate(R.id.action_userProfileFragment2_to_loginPageFragment2)
+        }
+        return view
     }
 
 }
