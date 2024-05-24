36f75a96b54c44a85ea41769c7afd58616834c17
k4mpa15 <kampa.kampa@wp.pl>
2024-05-24 23:18:59 +0200

    user profile i login

--- app/src/main/java/com/example/myapplication/LoginPageFragment.kt
+++ app/src/main/java/com/example/myapplication/LoginPageFragment.kt
@@ -1,24 +1,77 @@
 package com.example.myapplication
 
+import android.annotation.SuppressLint
+import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.EditText
 import androidx.navigation.fragment.findNavController
+import com.android.volley.Request
+import com.android.volley.RequestQueue
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+import org.json.JSONObject
 
 class LoginPageFragment : Fragment() {
-
+    private lateinit var queue: RequestQueue
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         val view =  inflater.inflate(R.layout.fragment_login_page, container, false)
+        val loginEditText = view.findViewById<EditText>(R.id.loginOrEmailEditText)
+        val passwordEditText = view.findViewById<EditText>(R.id.passwordEditText)
         val buttonlogin = view.findViewById<Button>(R.id.loginButton)
+        queue = Volley.newRequestQueue(requireContext())
+
+        loginEditText.setLayerType(View.LAYER_TYPE_SOFTWARE, null)
+        passwordEditText.setLayerType(View.LAYER_TYPE_SOFTWARE, null)
         buttonlogin.setOnClickListener{
-            findNavController().navigate(R.id.action_loginPageFragment2_to_homePageFragment)
+            val login = loginEditText.text.toString()
+            val haslo = passwordEditText.text.toString()
+
+            val url = "http://10.0.2.2:8081/login"
+            val credentialRequest = @SuppressLint("MissingInflatedId")
+            object : StringRequest(
+                Request.Method.POST, url,
+                { response ->
+                    try {
+                        Log.d("LoginPageFragment", "Response: $response")
+
+                        if (response == "Zalogowano pomyślnie.") {
+                            findNavController().navigate(R.id.action_loginPageFragment2_to_homePageFragment)
+                        } else {
+                            Log.e("LoginPageFragment", "Login failed: $response")
+                            val loginfailed = view.findViewById<Button>(R.id.loginfailed)
+                            loginfailed.text = "Bledne haslo!"
+                        }
+                    } catch (e: Exception) {
+                        Log.e("LoginPageFragment", "Error parsing JSON: ${e.message}")
+                    }
+                },
+                { error ->
+                    Log.e("LoginPageFragment", "Request error: ${error.message}")
+                    if (error.networkResponse != null && error.networkResponse.statusCode == 401) {
+                        val loginFailedButton = view.findViewById<Button>(R.id.loginfailed)
+                        loginFailedButton.text = "Błędne hasło!"
+                    }
+                }) {
+                override fun getParams(): Map<String, String> {
+                    val params = HashMap<String, String>()
+                    params["username"] = login
+                    params["password"] = haslo
+                    return params
+                }
+            }
+
+            queue.add(credentialRequest)
         }
+
         return view
     }
 
--- app/src/main/java/com/example/myapplication/UserProfileFragment.kt
+++ app/src/main/java/com/example/myapplication/UserProfileFragment.kt
@@ -7,15 +7,73 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import androidx.navigation.fragment.findNavController
+import android.annotation.SuppressLint
+import android.util.Log
+import android.widget.TextView
+import com.android.volley.Request
+import com.android.volley.RequestQueue
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+import org.json.JSONObject
 
 class UserProfileFragment : Fragment() {
-
-
+    private lateinit var queue: RequestQueue
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        queue = Volley.newRequestQueue(requireContext())
+    }
+    @SuppressLint("SetTextI18n", "MissingInflatedId")
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         val view =  inflater.inflate(R.layout.fragment_user_profile, container, false)
+        val pointsTextView = view.findViewById<TextView>(R.id.textViewPoints)
+        val loginTextView = view.findViewById<TextView>(R.id.editTextLogin)
+        val emailTextView = view.findViewById<TextView>(R.id.editTextEmail)
+        val url_points = "http://10.0.2.2:8081/user/points"
+        val stringRequest = StringRequest(
+            Request.Method.GET, url_points,
+            { response ->
+                try {
+                    val jsonObject = JSONObject(response)
+                    val points = jsonObject.getString("punkty")
+
+                    pointsTextView.text = points
+
+                } catch (e: Exception) {
+                    Log.e("BarFragment", "Error parsing JSON: ${e.message}")
+
+                }
+            },
+            { error ->
+                Log.e("BarFragment", "Request error: ${error.message}")
+            })
+
+        queue.add(stringRequest)
+        val url_credentials = "http://10.0.2.2:8081/user/profile"
+        val credentialRequest = StringRequest(
+            Request.Method.GET, url_credentials,
+            { response ->
+                try {
+                    val jsonObject = JSONObject(response)
+                    val login = jsonObject.getString("name")
+                    val email = jsonObject.getString("email")
+
+
+                    loginTextView.text = login
+                    emailTextView.text = email
+
+                } catch (e: Exception) {
+                    Log.e("BarFragment", "Error parsing JSON: ${e.message}")
+
+                }
+            },
+            { error ->
+                Log.e("BarFragment", "Request error: ${error.message}")
+            })
+
+        queue.add(credentialRequest)
         val buttonloggout = view.findViewById<Button>(R.id.buttonLogout)
         buttonloggout.setOnClickListener{
             findNavController().navigate(R.id.action_userProfileFragment2_to_loginPageFragment2)
