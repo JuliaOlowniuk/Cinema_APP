0ec0d14441816ac3a57e8575b9a12037cf949d24
k4mpa15 <kampa.kampa@wp.pl>
2024-05-24 20:07:07 +0200

    bar connection

--- app/src/main/java/com/example/myapplication/BarFragment.kt
+++ app/src/main/java/com/example/myapplication/BarFragment.kt
@@ -2,6 +2,7 @@ package com.example.myapplication
 
 import android.annotation.SuppressLint
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -22,27 +23,39 @@ class BarFragment : Fragment() {
         queue = Volley.newRequestQueue(requireContext())
     }
 
-    @SuppressLint("SetTextI18n")
+    @SuppressLint("SetTextI18n", "MissingInflatedId")
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         val view = inflater.inflate(R.layout.fragment_bar, container, false)
-        val popcornTextView = view.findViewById<TextView>(R.id.popcorn_text)
-        val url = "http://localhost:8081/prices/bar"
+        val popcornPriceTextView = view.findViewById<TextView>(R.id.popcorn_price)
+        val nachosPriceTextView = view.findViewById<TextView>(R.id.nachos_price)
+        val sokPriceTextView = view.findViewById<TextView>(R.id.sok_price)
+        val colaPriceTextView = view.findViewById<TextView>(R.id.cola_price)
+        val url = "http://10.0.2.2:8081/prices/bar"
+
         val stringRequest = StringRequest(
             Request.Method.GET, url,
             { response ->
                 try {
                     val jsonObject = JSONObject(response)
-                    val fact = jsonObject.getString("fact")
-                    popcornTextView.text = fact
+                    val pricepopcorn = jsonObject.getString("popcorn")
+                    val pricenachos = jsonObject.getString("nachos")
+                    val pricecola = jsonObject.getString("cola")
+                    val pricesok = jsonObject.getString("sok")
+                    popcornPriceTextView.text = pricepopcorn
+                    nachosPriceTextView.text = pricenachos
+                    colaPriceTextView.text = pricecola
+                    sokPriceTextView.text = pricesok
                 } catch (e: Exception) {
-                    popcornTextView.text = "Error parsing JSON!"
+                    Log.e("BarFragment", "Error parsing JSON: ${e.message}")
+                    popcornPriceTextView.text = "Error parsing JSON!"
                 }
             },
-            {
-                popcornTextView.text = "That didn't work!"
+            { error ->
+                Log.e("BarFragment", "Request error: ${error.message}")
+                popcornPriceTextView.text = "That didn't work!"
             })
 
         queue.add(stringRequest)
